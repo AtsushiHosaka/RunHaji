68a71328c98ec9715d8e5b3207a7a04bd61fdb10
Atsushi Hosaka <hosaka2917@gmail.com>
2025-11-13 14:32:41 +0900

    Initial commit

---
+++ RunHaji/RunHajiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunHajiApp.swift
+//  RunHaji
+//
+//  Created by 保坂篤志 on 2025/11/13.
+//
+
+import SwiftUI
+
+@main
+struct RunHajiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
